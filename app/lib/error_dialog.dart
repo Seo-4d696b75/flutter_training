988b960693185836cd1996cfa19f9bedf1294d11
senda_k <s.kaoru509@gmail.com>
2022-04-07 13:46:39 +0900

    追加：error dialog

---
+++ app/lib/error_dialog.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:hello_flutter/strings.dart';
+
+class ErrorDialog extends StatelessWidget {
+  const ErrorDialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text(Strings.errorDialogTitle),
+      content: const Text(Strings.errorDialogMessage),
+      actions: [
+        TextButton(
+          child: const Text(Strings.errorDialogNegative),
+          onPressed: () => Navigator.pop(context, ErrorDialogResult.close),
+        ),
+        TextButton(
+          child: const Text(Strings.errorDialogPositive),
+          onPressed: () => Navigator.pop(context, ErrorDialogResult.reload),
+        )
+      ],
+    );
+  }
+}
+
+enum ErrorDialogResult { close, reload }
