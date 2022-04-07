2874fbc70bd766a3ffe26000ed719ca5af338439
senda_k <s.kaoru509@gmail.com>
2022-04-07 13:46:29 +0900

    追加：ダイアログの文字列res

--- app/lib/strings.dart
+++ app/lib/strings.dart
@@ -1,3 +1,7 @@
 abstract class Strings {
   static const appName = "doroidtraining";
+  static const errorDialogTitle = "Error";
+  static const errorDialogMessage = "エラーが発生しました。";
+  static const errorDialogPositive = "Reload";
+  static const errorDialogNegative = "Close";
 }
