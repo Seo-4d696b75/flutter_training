8e9fc9a0da200c0ecb56409069c2c8a85db7aa64
senda_k <s.kaoru509@gmail.com>
2022-04-12 11:54:09 +0900

    追加：Localization(l10n)
    https://zenn.dev/aryzae/articles/0555959ac3800e

--- app/lib/data/strings.dart
+++
@@ -1,7 +0,0 @@
-abstract class Strings {
-  static const appName = "doroidtraining";
-  static const errorDialogTitle = "Error";
-  static const errorDialogMessage = "エラーが発生しました。";
-  static const errorDialogPositive = "Reload";
-  static const errorDialogNegative = "Close";
-}
